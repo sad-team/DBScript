97a7f6efc976b1d6421d2bfbd1e0d3ba1469214c
HondouEisuke00 <33284693+HondouEisuke00@users.noreply.github.com>
2019-04-19 21:07:10 +0800

    no message

---
+++ scripts/整库truncate.sql
@@ -0,0 +1,17 @@
+set FOREIGN_KEY_CHECKS=0;
+truncate table `academicInstitution`;
+truncate table `administrator`;
+truncate table `appeal`;
+truncate table `expert`;
+truncate table `follow`;
+truncate table `keyword`;
+truncate table `message`;
+truncate table `notice`;
+truncate table `order`;
+truncate table `paper`;
+truncate table `patent`;
+truncate table `project`;
+truncate table `report`;
+truncate table `resource`;
+truncate table `user`;
+set FOREIGN_KEY_CHECKS=1;
